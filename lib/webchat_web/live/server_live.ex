497e5e158ad08c3425ba77cd1710153da52c1d28
BillyM <billy.mercier@outlook.com>
2020-06-25 09:12:58 -0400

    server fetch for liveview

--- lib/webchat_web/live/server_live.ex
+++ lib/webchat_web/live/server_live.ex
@@ -1,16 +1,22 @@
 defmodule WebchatWeb.ServerLive do
   use WebchatWeb, :live_view
 
+  alias Webchat.Chat
 
   def mount(_params, _session, socket) do
-    socket = assign(socket, bob: "bob")
-    IO.inspect socket
+    servers = Chat.list_servers()
+    socket = assign(socket, servers: servers)
     {:ok, socket} 
   end
 
   def render(assigns) do
     ~L"""
-    <h1>hello</h1>
+    <%= for server <- @servers do %>
+      <h1><%= server.name %></h1>
+      <%= live_patch link_body(server),
+        to: Routes.live_path(
+          @socket
+    <% end %>
     """
   end
 
