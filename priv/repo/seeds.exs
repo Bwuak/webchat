0d3e451f1b08be0d5cb629e8daba8d94d2b5f253
Bwuak <billy.mercier@outlook.com>
2020-07-15 13:28:22 -0400

    seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -17,16 +17,14 @@ alias Webchat.Accounts.{User, Admin}
 # Create a user first then run the following
 user = Accounts.get_user_by(email: "admin@admin")
 
-# {:ok, server1} = Chat.create_server(%{name: "unoriginal server1", user_id: user.id})
-# {:ok, server2} = Chat.create_server(%{name: "another server", user_id: user.id})
+{:ok, server1} = Chat.create_server(%{name: "ServerTwo", user_id: user.id})
+{:ok, server2} = Chat.create_server(%{name: "Another server", user_id: user.id})
 # 
-# Chat.create_chatroom(server2.id, %{roomname: "First chatroom"}) 
-# Chat.create_chatroom(server1.id, %{roomname: "Some chatroom"})
-# Chat.create_chatroom(server1.id, %{roomname: "room"})
+Chat.create_chatroom(server2.id, %{roomname: "General"}) 
+Chat.create_chatroom(server1.id, %{roomname: "General"})
+Chat.create_chatroom(server1.id, %{roomname: "Another room"})
 
 # create a website admin
-user = Accounts.get_user!(user.id)
-{:ok, _admin} = Admin.changeset(%Admin{user_id: user.id}, %{}) |> Repo.insert()
+# user = Accounts.get_user!(user.id)
+# {:ok, _admin} = Admin.changeset(%Admin{user_id: user.id}, %{}) |> Repo.insert()
 
-ans = Accounts.is_admin?(user)
-IO.inspect ans
