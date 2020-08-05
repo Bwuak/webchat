00d389cc20557fa8b2be50dccdbdd91afbcd8112
Bwuak <billy.mercier@outlook.com>
2020-08-05 11:07:33 -0400

    mount

--- lib/webchat_web/live/chat/chatroom_creation_component.ex
+++ lib/webchat_web/live/chat/chatroom_creation_component.ex
@@ -32,10 +32,6 @@ defmodule WebchatWeb.Chat.ChatroomCreationComponent do
     """
   end
 
-  def mount(socket) do
-    {:ok, socket} 
-  end
-
   # called after mount, has assigns sent to socket
   def update(assigns, socket) do
     {:ok, assign(socket,
