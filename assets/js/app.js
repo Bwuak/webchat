ab60be1abd0726da4e07ef28f0725548180a2b14
Bwuak <billy.mercier@outlook.com>
2020-08-17 15:07:34 -0400

    adding nprogress

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,6 +15,7 @@ import "../css/app.scss"
 import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
+import NProgress from "nprogress"
 
 
 import WebchatRequests from "./webchat_requests"
@@ -26,6 +27,10 @@ import Controller from "./chat_controller"
 
 if(document.getElementById("chatroom")) {
   let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
+
+  // Progress bar
+  window.addEventListener("phx:page-loading-start", info => NProgress.start())
+  window.addEventListener("phx:page-loading-stop", info => NProgress.done())
 
   // liveview socket
   let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks.Hooks, params: {_csrf_token: csrfToken}})
