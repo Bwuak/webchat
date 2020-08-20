6ec6e117e5ddc8ef44fa563bc848e4466a128cf5
Bwuak <billy.mercier@outlook.com>
2020-08-20 13:06:47 -0400

    messages comments

--- lib/webchat/chat/messages.ex
+++ lib/webchat/chat/messages.ex
@@ -7,12 +7,15 @@ defmodule Webchat.Chat.Messages do
   alias Webchat.Chat.Models.Message
 
 
-  @high_oldest_seen_id 90000000000
-  def list_for(room, oldest \\ @high_oldest_seen_id) 
 
   @doc """
   gives up to 50 messages older than oldest seen, chatroom history
+
+  @high_oldest_seen_id replaces oldest id when none provided
   """
+  @high_oldest_seen_id 90000000000
+  def list_for(room, oldest \\ @high_oldest_seen_id) 
+
   def list_for(%Chatroom{} = room, oldest_id) 
       when is_integer(oldest_id) do
     Repo.all(
@@ -25,13 +28,13 @@ defmodule Webchat.Chat.Messages do
     |> Enum.reverse
   end
 
-  @doc """
-  Gives 50 messages from last message
-  """
   def list_for(%Chatroom{} = room, _) do 
     list_for(room, @high_oldest_seen_id)
   end
 
+  @doc """
+  A user creates a new message inside a chatroom
+  """
   def add_message(%User{id: user_id}, chatroom_id, attrs) do
     %Message{user_id: user_id, chatroom_id: chatroom_id}
     |> Message.changeset(attrs)
